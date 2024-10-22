9592afec02150f28bb6d794104050e57cd902a59
dunglt94 <tuandung9594@gmail.com>
2024-10-22 16:18:28 +0700

    first add

---
+++ src/TestMyLinkedList.java
@@ -0,0 +1,18 @@
+public class TestMyLinkedList {
+    public static void main(String[] args) {
+        System.out.println("/=/=/=/= TESTING /=/=/=/=");
+        MyLinkedList ll = new MyLinkedList(10);
+        ll.addFirst(9);
+        ll.addFirst(8);
+        ll.addFirst(7);
+        ll.addFirst(6);
+        ll.addFirst(5);
+        ll.addFirst(1);
+
+        ll.add(1,2);
+        ll.add(2,3);
+        ll.add(3,4);
+
+        ll.printList();
+    }
+}
